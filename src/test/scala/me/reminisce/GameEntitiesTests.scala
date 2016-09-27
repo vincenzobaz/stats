c9c641b410e760eaab50dc7ade0e70aac527f128
Jeremy Rabasco <rabasco.jeremy@gmail.com>
2016-09-27 19:49:40 +0200

    Fixes the tests:
    - Replace ```==``` with a proper field by field comparison on the
      GameEntitiesTest
    - Fixes the forkbomb: do not generate stats for 45 years' worth of stats

--- src/test/scala/me/reminisce/GameEntitiesTests.scala
+++ src/test/scala/me/reminisce/GameEntitiesTests.scala
@@ -1,24 +1,97 @@
 package me.reminisce
 
 import java.io.InputStream
+
 import me.reminisce.server.GameEntities._
-import me.reminisce.server.{SubjectSerializer, QuestionSerializer}
-import org.json4s.JDouble
-import org.json4s.JInt
-import org.json4s.JsonAST.JArray
-import org.json4s.JsonAST.JField
-import org.json4s.JsonAST.JObject
-import org.json4s.JsonAST.JString
-import org.scalatest.FunSuite
+import me.reminisce.server.{QuestionSerializer, SubjectSerializer}
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
+import org.scalatest.FunSuite
 
 class GameEntitiesTests extends FunSuite {
+
+  def assertContains(expected: FBFrom, actual: Option[FBFrom]): Unit = {
+    actual match {
+      case Some(from) =>
+        assert(from.userId == expected.userId)
+        assert(from.userName == expected.userName)
+      case None =>
+        fail("From is None.")
+    }
+  }
+
+  def assertEqualsOption[T](expectedOpt: Option[T], actualOpt: Option[T]): Unit = expectedOpt match {
+    case Some(expected) =>
+      assert(actualOpt.contains(expected))
+    case None =>
+      assert(actualOpt.isEmpty)
+  }
+
+  def assertEqualsNoQuestion(expected: Game, actual: Game): Unit = {
+
+    def assertEquals(expectedPlayerMoves: List[Move], actualPlayerMoves: List[Move]): Unit = {
+      assert(expectedPlayerMoves.length == actualPlayerMoves.length)
+      expectedPlayerMoves.zip(actualPlayerMoves).foreach {
+        case (expectedMove, actualMove) =>
+          assert(expectedMove.column == actualMove.column)
+          assert(expectedMove.row == actualMove.row)
+      }
+    }
+
+    assert(expected._id == actual._id)
+    assert(expected.creationTime == actual.creationTime)
+    assert(expected.player1 == actual.player1)
+    assert(expected.player2 == actual.player2)
+    assert(expected.player1Scores == actual.player1Scores)
+    assert(expected.player2Scores == actual.player2Scores)
+    assert(expected.playerTurn == actual.playerTurn)
+    assert(expected.status == actual.status)
+    assert(expected.wonBy == actual.wonBy)
+
+    assert(expected.boardState.length == actual.boardState.length)
+    expected.boardState.zip(actual.boardState).foreach {
+      case (expectedScores, actualScores) =>
+        assert(expectedScores.length == actualScores.length)
+        expectedScores.zip(actualScores).foreach {
+          case (expectedScore, actualScore) =>
+            assert(expectedScore.player == actualScore.player)
+            assert(expectedScore.score == actualScore.score)
+        }
+    }
+
+    assertEquals(expected.player1AvailableMoves, actual.player1AvailableMoves)
+    assertEquals(expected.player2AvailableMoves, actual.player2AvailableMoves)
+
+    assertEqualsNoQuestion(expected.player1Board, actual.player1Board)
+    assertEqualsNoQuestion(expected.player2Board, actual.player2Board)
+  }
+
+  def assertEqualsNoQuestion(expected: Board, actual: Board): Unit = {
+    assert(expected._id == actual._id)
+    assert(expected.userId == actual.userId)
+    assert(expected.tiles.length == actual.tiles.length)
+
+    expected.tiles.zip(actual.tiles).foreach {
+      case (expectedTile, actualTile) =>
+        assertEqualsNoQuestion(expectedTile, actualTile)
+    }
+  }
+
+  def assertEqualsNoQuestion(expected: Tile, actual: Tile): Unit = {
+    assert(expected.`type` == actual.`type`)
+    assert(expected._id == actual._id)
+    assert(actual.question1.isEmpty)
+    assert(actual.question2.isEmpty)
+    assert(actual.question3.isEmpty)
+    assert(expected.score == actual.score)
+    assert(expected.answered == actual.answered)
+    assert(expected.disabled == actual.disabled)
+  }
 
   test("PageSubject") {
     implicit val formats = DefaultFormats
 
-    val pageSubject =
+    val page =
       """{
              "name": "Blood Bowl",
              "pageId": "13590131663",
@@ -26,11 +99,13 @@ class GameEntitiesTests extends FunSuite {
              "type": "Page"
            }"""
 
-    assert(parse(pageSubject).extract[PageSubject] == PageSubject("Blood Bowl",
-      "13590131663",
-      Some("https://scontent.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/1929960_13590436663_114_n.jpg?oh=25eae23b71e482c85c7fb68d768ab4fa&oe=5632DFF0"),
-      SubjectType.PageSubject)
-    )
+    val pageSubject = parse(page).extract[PageSubject]
+
+    assert(pageSubject.name == "Blood Bowl")
+    assert(pageSubject.pageId == "13590131663")
+    assert(pageSubject.`type` == SubjectType.PageSubject)
+    assert(pageSubject.photoUrl.contains("https://scontent.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/1929960_13590436663_" +
+      "114_n.jpg?oh=25eae23b71e482c85c7fb68d768ab4fa&oe=5632DFF0"))
   }
 
   test("TextPostSubject") {
@@ -42,8 +117,11 @@ class GameEntitiesTests extends FunSuite {
             "type": "TextPost"
           }"""
 
-    assert(parse(textPost).extract[TextPostSubject] == TextPostSubject("I will post some weird things in the future..."
-      , SubjectType.TextPost, None))
+    val postSubject = parse(textPost).extract[TextPostSubject]
+
+    assert(postSubject.text == "I will post some weird things in the future...")
+    assert(postSubject.`type` == SubjectType.TextPost)
+    assert(postSubject.from.isEmpty)
   }
 
   test("ImagePostSubject") {
@@ -57,9 +135,14 @@ class GameEntitiesTests extends FunSuite {
             "type": "ImagePost"
           }"""
 
-    assert(parse(imagePost).extract[ImagePostSubject] == ImagePostSubject("You changed your profile picture.",
-      Some("https://scontent.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/p180x540/396092_10151324342204968_433100355_n.jpg?oh=107c5dc045e438b5273a14d568c934c5&oe=55F0453B"),
-      Some("https://www.facebook.com/photo.php?fbid=10151324342204968&set=a.499536429967.288132.656209967&type=1"), SubjectType.ImagePost, None))
+    val imagePostSubject = parse(imagePost).extract[ImagePostSubject]
+
+    assert(imagePostSubject.text == "You changed your profile picture.")
+    assert(imagePostSubject.imageUrl.contains("https://scontent.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/p180x540/" +
+      "396092_10151324342204968_433100355_n.jpg?oh=107c5dc045e438b5273a14d568c934c5&oe=55F0453B"))
+    assert(imagePostSubject.facebookImageUrl.contains("https://www.facebook.com/photo.php?fbid=10151324342204968&set=a.499536429967.288132.656209967&type=1"))
+    assert(imagePostSubject.`type` == SubjectType.ImagePost)
+    assert(imagePostSubject.from.isEmpty)
   }
 
   test("VideoPostSubject") {
@@ -77,10 +160,15 @@ class GameEntitiesTests extends FunSuite {
             }
           }"""
 
-    assert(parse(videoPost).extract[VideoPostSubject] == VideoPostSubject("Roger Küng shared a link.",
-      Some("https://external.xx.fbcdn.net/safe_image.php?d=AQBgK6vGG5nYP2hm&w=720&h=720&url=http%3A%2F%2Fi.ytimg.com%2Fvi%2FfzMhh8zhTiY%2F0.jpg&cfs=1"),
-      Some("http://www.youtube.com/watch?v=fzMhh8zhTiY"), SubjectType.VideoPost,
-      Some(FBFrom("656209967", "Roger Küng"))))
+    val videoPostSubject = parse(videoPost).extract[VideoPostSubject]
+
+    assert(videoPostSubject.text == "Roger Küng shared a link.")
+    assert(videoPostSubject.thumbnailUrl.contains("https://external.xx.fbcdn.net/safe_image.php?d=AQBgK6vGG5nYP2hm&w=720" +
+      "&h=720&url=http%3A%2F%2Fi.ytimg.com%2Fvi%2FfzMhh8zhTiY%2F0.jpg&cfs=1"))
+    assert(videoPostSubject.url.contains("http://www.youtube.com/watch?v=fzMhh8zhTiY"))
+    assert(videoPostSubject.`type` == SubjectType.VideoPost)
+
+    assertContains(FBFrom("656209967", "Roger Küng"), videoPostSubject.from)
   }
 
   test("LinkPostSubject") {
@@ -98,10 +186,14 @@ class GameEntitiesTests extends FunSuite {
             }
          }"""
 
-    assert(parse(linkPost).extract[LinkPostSubject] == LinkPostSubject("I guess the U.S. Healthcare state needs some readjustment",
-      Some("https://external.xx.fbcdn.net/safe_image.php?d=AQBP8YvTVf_VKUMb&w=720&h=720&url=http%3A%2F%2Fi.imgur.com%2FBbQgU8a.jpg&cfs=1"),
-      Some("http://redditpics.fpapps.com/?thingid=t3_3dngld&url=http%3A%2F%2Fi.imgur.com%2FBbQgU8a.jpg"), SubjectType.LinkPost,
-      Some(FBFrom("656209967", "Roger Küng"))))
+    val linkPostSubject = parse(linkPost).extract[LinkPostSubject]
+
+    assert(linkPostSubject.text == "I guess the U.S. Healthcare state needs some readjustment")
+    assert(linkPostSubject.thumbnailUrl.contains("https://external.xx.fbcdn.net/safe_image.php?d=AQBP8YvTVf_VKUMb&w=720&" +
+      "h=720&url=http%3A%2F%2Fi.imgur.com%2FBbQgU8a.jpg&cfs=1"))
+    assert(linkPostSubject.url.contains("http://redditpics.fpapps.com/?thingid=t3_3dngld&url=http%3A%2F%2Fi.imgur.com%2FBbQgU8a.jpg"))
+    assert(linkPostSubject.`type` == SubjectType.LinkPost)
+    assertContains(FBFrom("656209967", "Roger Küng"), linkPostSubject.from)
   }
 
   test("CommentSubject") {
@@ -117,8 +209,11 @@ class GameEntitiesTests extends FunSuite {
             "type": "Comment"
           }"""
 
-    assert(parse(comment).extract[CommentSubject] == CommentSubject("I just noticed that. How is it scientific if it's not your usual and natural behavior ? ;)",
-      None, SubjectType.CommentSubject))
+    val commentSubject = parse(comment).extract[CommentSubject]
+
+    assert(commentSubject.comment == "I just noticed that. How is it scientific if it's not your usual and natural behavior ? ;)")
+    assert(commentSubject.`type` == SubjectType.CommentSubject)
+    assert(commentSubject.post.isEmpty)
   }
 
 
@@ -139,9 +234,18 @@ class GameEntitiesTests extends FunSuite {
             "kind": "Timeline"
           }"""
 
-    assert(parse(question).extract[TimelineQuestion] == TimelineQuestion(None,
-      "2015-06-03T11:41:09+0000", "2015-06-07T11:41:09+0000", "2015-06-03T11:41:09+0000",
-      "Day", 1, 0, "2015-06-07T11:41:09+0000", QuestionKind.Timeline, "TLWhenDidYouShareThisPost"))
+    val parsedQuestion = parse(question).extract[TimelineQuestion]
+
+    assert(parsedQuestion.subject.isEmpty)
+    assert(parsedQuestion.min == "2015-06-03T11:41:09+0000")
+    assert(parsedQuestion.max == "2015-06-07T11:41:09+0000")
+    assert(parsedQuestion.default == "2015-06-03T11:41:09+0000")
+    assert(parsedQuestion.unit == "Day")
+    assert(parsedQuestion.step == 1)
+    assert(parsedQuestion.threshold == 0)
+    assert(parsedQuestion.answer == "2015-06-07T11:41:09+0000")
+    assert(parsedQuestion.kind == QuestionKind.Timeline)
+    assert(parsedQuestion.`type` == "TLWhenDidYouShareThisPost")
   }
 
   test("MultipleChoiceQuestion") {
@@ -184,13 +288,34 @@ class GameEntitiesTests extends FunSuite {
       None, List(Possibility("Maria Maria", None, Some("10152584486929069"), None), Possibility("Zelal Al-Shemmery", None, Some("768569646537959"), None)
         , Possibility("Christian M. Schmid", None, Some("10201396264188446"), None),
         Possibility("Michalina Pacholska", None, Some("714035445332109"), None)), 1))
+
+    val parsedQuestion = parse(question).extract[MultipleChoiceQuestion]
+
+    assert(parsedQuestion.kind == QuestionKind.MultipleChoice)
+    assert(parsedQuestion.`type` == "MCWhoLikedYourPost")
+    assert(parsedQuestion.subject.isEmpty)
+
+    val expectedChoices = List(Possibility("Maria Maria", None, Some("10152584486929069"), None),
+      Possibility("Zelal Al-Shemmery", None, Some("768569646537959"), None),
+      Possibility("Christian M. Schmid", None, Some("10201396264188446"), None),
+      Possibility("Michalina Pacholska", None, Some("714035445332109"), None))
+
+    assert(parsedQuestion.choices.length == expectedChoices.length)
+
+    parsedQuestion.choices.zip(expectedChoices).foreach {
+      case (expected, actual) =>
+        assert(expected.text == actual.text)
+        assertEqualsOption(expected.fbId, actual.fbId)
+        assertEqualsOption(expected.imageUrl, actual.imageUrl)
+        assertEqualsOption(expected.pageId, actual.pageId)
+    }
   }
 
   test("GeolocationQuestion") {
     implicit val formats = DefaultFormats
 
     val question =
-                         """{
+      """{
                                "subject": {},
                                "range": 0.02612831795,
                                "defaultLocation": {
@@ -209,6 +334,16 @@ class GameEntitiesTests extends FunSuite {
       Location(46.54730608686859, 6.57538616738275), Location(46.519681242464, 6.5717116820427),
       "GeoWhatCoordinatesWereYouAt", QuestionKind.Geolocation))
 
+    val parsedQuestion = parse(question).extract[GeolocationQuestion]
+
+    assert(parsedQuestion.subject.isEmpty)
+    assert(parsedQuestion.range == 0.02612831795)
+    assert(parsedQuestion.defaultLocation.latitude == 46.54730608686859)
+    assert(parsedQuestion.defaultLocation.longitude == 6.57538616738275)
+    assert(parsedQuestion.answer.latitude == 46.519681242464)
+    assert(parsedQuestion.answer.longitude == 6.5717116820427)
+    assert(parsedQuestion.`type` == "GeoWhatCoordinatesWereYouAt")
+    assert(parsedQuestion.kind == QuestionKind.Geolocation)
   }
 
   test("Tile") {
@@ -226,14 +361,17 @@ class GameEntitiesTests extends FunSuite {
       "disabled": false
     }"""
 
-    assert(parse(tile).extract[Tile] == Tile("Timeline", "87c565b56f7fc92ff8617c2b", None, None, None, 0, false, false))
+    val parsedTile = parse(tile).extract[Tile]
+    val expectedTile = Tile("Timeline", "87c565b56f7fc92ff8617c2b", None, None, None, 0, answered = false, disabled = false)
+
+    assertEqualsNoQuestion(expectedTile, parsedTile)
   }
 
   test("Board") {
     implicit val formats = DefaultFormats
 
     val board =
-                 """{
+      """{
                       "userId": "MmYXQ5EKSgdjzP3uJ",
                       "tiles": [
                         {
@@ -281,18 +419,22 @@ class GameEntitiesTests extends FunSuite {
                       "_id": "wQz5dAXnpDcyvrJud"
                     }"""
 
-    assert(parse(board).extract[Board] == Board("MmYXQ5EKSgdjzP3uJ", List(Tile("MultipleChoice", "79c2378123949e4386631412", None, None, None, 0, false, false),
-      Tile("Order", "9119c06edf2e473772ea37a2", None, None, None, 0, false, false),
-      Tile("Order", "b7893b57c6ba4350fb3d977a", None, None, None, 0, false, false),
-      Tile("MultipleChoice", "6cb42b812a48c42cc4b0024c", None, None, None, 0, false, false)),
-      "wQz5dAXnpDcyvrJud"))
+    val actualBoard = parse(board).extract[Board]
+    val expectedBoard = Board("MmYXQ5EKSgdjzP3uJ",
+      List(Tile("MultipleChoice", "79c2378123949e4386631412", None, None, None, 0, answered = false, disabled = false),
+        Tile("Order", "9119c06edf2e473772ea37a2", None, None, None, 0, answered = false, disabled = false),
+        Tile("Order", "b7893b57c6ba4350fb3d977a", None, None, None, 0, answered = false, disabled = false),
+        Tile("MultipleChoice", "6cb42b812a48c42cc4b0024c", None, None, None, 0, answered = false, disabled = false)),
+      "wQz5dAXnpDcyvrJud")
+
+    assertEqualsNoQuestion(expectedBoard, actualBoard)
   }
 
   test("Game") {
     implicit val formats = DefaultFormats
 
     val game =
-              """{
+      """{
                    "_id": "qS3MNajyD4qd2RYpy",
                    "player1": "MmYXQ5EKSgdjzP3uJ",
                    "player2": "wucQxKHvs5W9Ao9y9",
@@ -449,16 +591,19 @@ class GameEntitiesTests extends FunSuite {
                    "creationTime": 1456062881638
                  }"""
 
-    assert(parse(game).extract[Game] == Game("qS3MNajyD4qd2RYpy", "MmYXQ5EKSgdjzP3uJ", "wucQxKHvs5W9Ao9y9",
-      Board("MmYXQ5EKSgdjzP3uJ", List(Tile("Order", "9119c06edf2e473772ea37a2", None, None, None, 0, false, false),
-        Tile("Order", "b7893b57c6ba4350fb3d977a", None, None, None, 0, false, false),
-        Tile("MultipleChoice", "6cb42b812a48c42cc4b0024c", None, None, None, 0, false, false)), "wQz5dAXnpDcyvrJud"),
-      Board("wucQxKHvs5W9Ao9y9", List(Tile("Geolocation", "7fe68a7b1edf66b0dae1aaac", None, None, None, 0, false, false),
-        Tile("MultipleChoice", "099a1cec7b072135919f04fb", None, None, None, 0, false, false)), "fAYrgj74h9dv9Dhmq"),
+    val expectedGame = Game("qS3MNajyD4qd2RYpy", "MmYXQ5EKSgdjzP3uJ", "wucQxKHvs5W9Ao9y9",
+      Board("MmYXQ5EKSgdjzP3uJ", List(Tile("Order", "9119c06edf2e473772ea37a2", None, None, None, 0, answered = false, disabled = false),
+        Tile("Order", "b7893b57c6ba4350fb3d977a", None, None, None, 0, answered = false, disabled = false),
+        Tile("MultipleChoice", "6cb42b812a48c42cc4b0024c", None, None, None, 0, answered = false, disabled = false)), "wQz5dAXnpDcyvrJud"),
+      Board("wucQxKHvs5W9Ao9y9", List(Tile("Geolocation", "7fe68a7b1edf66b0dae1aaac", None, None, None, 0, answered = false, disabled = false),
+        Tile("MultipleChoice", "099a1cec7b072135919f04fb", None, None, None, 0, answered = false, disabled = false)), "fAYrgj74h9dv9Dhmq"),
       "ended", 1, 3, 7, List(List(Score(1, 0), Score(2, 0), Score(2, 3)), List(Score(2, 0), Score(2, 3), Score(1, 0)),
         List(Score(2, 1), Score(1, 1), Score(0, 0))), List(Move(0, 1), Move(1, 0), Move(2, 0), Move(2, 2)), List(Move(0, 0),
-        Move(1, 2), Move(2, 1), Move(2, 2)), 2, 68968294))
+        Move(1, 2), Move(2, 1), Move(2, 2)), 2, 1456062881638L)
 
+    val actualGame = parse(game).extract[Game]
+
+    assertEqualsNoQuestion(expectedGame, actualGame)
   }
 
   test("json from file") {
--- src/test/scala/me/reminisce/server/ServerServiceActorSpec.scala
+++ src/test/scala/me/reminisce/server/ServerServiceActorSpec.scala
@@ -1,59 +1,48 @@
 package me.reminisce.server
 
 import java.util.concurrent.TimeUnit
-import scala.concurrent.duration.Duration
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.language.postfixOps
+
 import akka.testkit.TestActorRef
 import me.reminisce.database.DatabaseTester
 import me.reminisce.statistics.StatisticEntities._
-import me.reminisce.server.GameEntities._
-import me.reminisce.server.JsonEntity._
-import org.scalatest.DoNotDiscover
-import spray.client.pipelining._
+import org.json4s.JsonAST.{JObject, JString}
+import org.json4s.jackson.JsonMethods._
+import org.json4s.{DefaultFormats, Formats, _}
+import spray.http.ContentTypes._
 import spray.http._
-import HttpMethods._
-import HttpHeaders._
-import ContentTypes._
-import com.github.nscala_time.time.Imports._
-import org.json4s.jackson.JsonMethods._
-import org.json4s.{DefaultFormats, Formats}
-import org.json4s.{JDouble, JInt}
-import org.json4s.JsonAST.{JArray, JField, JObject, JString}
-import org.json4s._
-import org.json4s.jackson.JsonMethods._
 
-//@DoNotDiscover
+import scala.concurrent.duration.Duration
+import scala.language.postfixOps
+
 class ServerServiceActorSpec extends DatabaseTester("ServerServiceActorSpec") {
 
-  case class SimpleMessageFormat(message: String)
-
   val testService = TestActorRef[ServerServiceActor]
-
-  implicit def json4sFormats: Formats = DefaultFormats
-  
   val statsTest = StatResponse("userID123", FrequencyOfPlays())
   val randomID: String = java.util.UUID.randomUUID.toString
   val randomUser1: String = java.util.UUID.randomUUID.toString
   val randomUser2: String = java.util.UUID.randomUUID.toString
   val urlInsert = "/insertEntity"
+
+  implicit def json4sFormats: Formats = DefaultFormats
+
+  case class SimpleMessageFormat(message: String)
+
   "ServerServiceActor" must {
 
     "try to insert a Game." in {
 
-      val gameJson : String = JsonEntity.game(randomID, randomUser1, randomUser2)
-      val postRequest = new HttpRequest(        
-                          method = HttpMethods.POST,
-                          uri = urlInsert, 
-                          entity = HttpEntity(`application/json`, gameJson)
-                          )
-      
+      val gameJson: String = JsonEntity.game(randomID, randomUser1, randomUser2)
+      val postRequest = new HttpRequest(
+        method = HttpMethods.POST,
+        uri = urlInsert,
+        entity = HttpEntity(`application/json`, gameJson)
+      )
+
       assert(postRequest.method == HttpMethods.POST)
       testService ! postRequest
-      
+
       val responseOpt = Option(receiveOne(Duration(10, TimeUnit.SECONDS)))
-      
+
       responseOpt match {
         case Some(response) =>
           assert(response.isInstanceOf[HttpResponse])
@@ -61,11 +50,11 @@ class ServerServiceActorSpec extends DatabaseTester("ServerServiceActorSpec") {
           val httpResponse = response.asInstanceOf[HttpResponse]
           val json = parse(httpResponse.entity.data.asString)
           json match {
-            case JObject(List((k, msg))) =>  
+            case JObject(List((k, msg))) =>
               assert(k == "status")
-              assert(msg== JString("Done"))
+              assert(msg == JString("Done"))
             case _ => fail("Fail to insert")
-          }          
+          }
         case None =>
           fail("Response is not defined.")
       }
@@ -82,11 +71,11 @@ class ServerServiceActorSpec extends DatabaseTester("ServerServiceActorSpec") {
           val httpResponse = response.asInstanceOf[HttpResponse]
           val json = parse(httpResponse.entity.data.asString)
           json match {
-            case JObject(List((k, msg))) =>  
+            case JObject(List((k, msg))) =>
               assert(k == "message")
               assert(msg == JString(s"Statistics not found for NOT${randomUser1}"))
             case _ => fail("Response is not defined.")
-          }         
+          }
         case None =>
           fail("No response")
       }
@@ -109,38 +98,38 @@ class ServerServiceActorSpec extends DatabaseTester("ServerServiceActorSpec") {
             case _ =>
               fail("Response isn't a Stats object")
           }
-          //TODO create deserializer for Stats
+        //TODO create deserializer for Stats
         case None =>
           fail("Response is not defined.")
       }
-    } 
+    }
     "try to insert a duplicate game" in {
-      val gameJson : String = JsonEntity.game(randomID, randomUser1, randomUser2)
+      val gameJson: String = JsonEntity.game(randomID, randomUser1, randomUser2)
 
-      val postRequest = new HttpRequest(        
-                          method = HttpMethods.POST,
-                          uri = urlInsert, 
-                          entity = HttpEntity(`application/json`, gameJson)
-                          )
+      val postRequest = new HttpRequest(
+        method = HttpMethods.POST,
+        uri = urlInsert,
+        entity = HttpEntity(`application/json`, gameJson)
+      )
       assert(postRequest.method == HttpMethods.POST)
       testService ! postRequest
-      
+
       val responseOpt = Option(receiveOne(Duration(10, TimeUnit.SECONDS)))
-      
+
       responseOpt match {
         case Some(response) =>
           assert(response.isInstanceOf[HttpResponse])
           val httpResponse = response.asInstanceOf[HttpResponse]
           val json = parse(httpResponse.entity.data.asString)
           json match {
-            case JObject(a) =>  
+            case JObject(a) =>
               assert(a.head._1 == "status")
               assert(a.head._2 == JString("Aborted"))
             case _ => fail("Fail to insert")
-          }          
+          }
         case None =>
           fail("Response is not defined.")
       }
-    } 
+    }
   }
 }
