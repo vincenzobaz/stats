6fe3d6b7df03b93af333c9f610a699086b52bab6
Audrey Loeffel <audrey.loeffel@epfl.ch>
2016-10-26 14:48:30 +0200

    Fix JSON serializer issue and update the computation

--- src/main/scala/me/reminisce/model/RetrievingMessages.scala
+++ src/main/scala/me/reminisce/model/RetrievingMessages.scala
@@ -7,8 +7,7 @@ import scala.concurrent.Future
 
 object RetrievingMessages {
 
-  case class RetrieveStats(userID: String, from :Option[DateTime], to: Option[DateTime])
-  extends RestMessage
+  case class RetrieveStats(userID: String, from :Option[DateTime], to: Option[DateTime], limit: Option[Int]) extends RestMessage
 
   case class UserNotFound(message: String) extends RestMessage
   case class StatsRetrieved(stats: List[StatsEntities]) extends RestMessage
