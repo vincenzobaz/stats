75b48e49ae80f97ef696b34f61841d88680ac1b4
Jeremy Rabasco <rabasco.jeremy@gmail.com>
2016-11-01 11:50:42 +0100

    Update scala version

--- src/main/scala/me/reminisce/server/ApplicationConfiguration.scala
+++ src/main/scala/me/reminisce/server/ApplicationConfiguration.scala
@@ -5,15 +5,9 @@ import scala.util.Properties._
 
 object ApplicationConfiguration {
 
-
-  val appMode = "DEV"
-
   val hostName = "localhost"
   
-  // Connect to the local DB
-  val dbName = "mockDB"
-
-//  val dbName = "reminisce_stats"
+	val dbName = "reminisce_stats"
 
   val serverPort = 7777
   val mongoHost = envOrElse("MONGODB_HOST", hostName)
